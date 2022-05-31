496267895cb35cf2d8825d093a05fcbb9bb279f7
theebak nanthapalan <tbnan@hotmail.com>
2022-05-31 20:53:39 +0100

    taxifaremodel mlflow

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -5,6 +5,9 @@ from sklearn.compose import ColumnTransformer
 from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
 
 class Trainer():
     def __init__(self, X, y):
@@ -15,6 +18,32 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+
+        self.uri = "https://mlflow.lewagon.ai/"
+        self.experiment_name = "[UK] [London] [theebak31] TaxiFareModel + v1.2"
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.uri)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
 
     def set_pipeline(self):
         print("defines the pipeline as a class attribute")
@@ -42,7 +71,13 @@ class Trainer():
     def evaluate(self, X_test, y_test):
         print("evaluates the pipeline on df_test and return the RMSE")
         y_pred = self.pipeline.predict(X_test)
-        return np.sqrt(((y_pred - y_test)**2).mean())
+
+        rmse = np.sqrt(((y_pred - y_test)**2).mean())
+        self.mlflow_log_metric("RMSE", rmse)
+
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        pass
 
 
 if __name__ == "__main__":
